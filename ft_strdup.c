6f15cb08df66dfef442127b6bf891a56e2974f9b
Manuel Ignacio Luis Fuentes <manuelluisf96@gmail.com>
2022-06-17 23:50:06 +0200

    malloc

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/17 23:19:15 by marvin            #+#    #+#             */
-/*   Updated: 2022/06/17 23:19:15 by marvin           ###   ########.fr       */
+/*   Created: 2022/06/17 23:24:44 by marvin            #+#    #+#             */
+/*   Updated: 2022/06/17 23:24:44 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *calloc(size_t n, size_t size)
+char *strdup(const char *str)
 {
-	void    *ptr;
-	ptr = malloc(n * size);
-	if (ptr == NULL)
-		return (ptr);
-	ft_bzero(ptr, size * n);
-	return (ptr);
+	size_t  slen;
+	char *aux;
+
+	slen = ft_strlen(str);
+	aux = (char *)malloc(sizeof(char) * (slen + 1));
+	if (!aux)
+		return(NULL);
+	ft_memcpy(aux, str, slen - 1);
+	return (aux);
 }
