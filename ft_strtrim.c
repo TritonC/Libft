2ea0627cac88b2e8cf799ad8a4a7f98d34c05d02
TritonC <manuelluisf96@gmail.com>
2022-06-18 16:44:12 +0200

    new try

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,40 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/17 23:24:44 by marvin            #+#    #+#             */
-/*   Updated: 2022/06/17 23:24:44 by marvin           ###   ########.fr       */
+/*   Created: 2022/06/18 15:55:01 by marvin            #+#    #+#             */
+/*   Updated: 2022/06/18 15:55:01 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*ft_strcpy(char *dest, char *src)
-{
-	int	count;
-
-	count = 0;
-	while (src[count])
-	{
-		dest[count] = src[count];
-		count++;
-	}
-	dest[count] = '\0';
-	return (dest);
+static char ft_isspace(char *str) 
+{                   //copy of the isspace function to remove spaces
+    size_t i;
+    
+    i = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+	i++;
 }
 
-char *strdup(const char *str)
-{
-	size_t  slen;
-	char *aux;
-
-	slen = ft_strlen(str);
-	aux = (char *)malloc(sizeof(char) * (slen + 1));
-	if (!aux)
-		return(NULL);
-	ft_strcpy(aux, str);
-	return (aux);
-}
+char *ft_strtrim(char const *s1, char const *set)
