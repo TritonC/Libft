0cc632a43b9b0d37523365de6bfb2e9e5f3bfa02
Manuel Ignacio Luis Fuentes <manuelluisf96@gmail.com>
2022-06-19 23:27:15 +0200

    adding function

--- libft.h
+++ libft.h
@@ -40,5 +40,6 @@ int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*strdup(const char *str);
 void	*calloc(size_t n, size_t size);
+char	*ft_strjoin(char const *dest, char const *src);
 
 #endif
