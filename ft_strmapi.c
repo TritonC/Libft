37778e37eea0f756324ef7520cbf6441d322bb02
Manuel Ignacio Luis Fuentes <manuelluisf96@gmail.com>
2022-06-23 19:15:39 +0200

    change

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,36 +14,33 @@
 
 static char *dinamic_str(size_t size)
 {
-    int     count;
-    char    *str;
-    count = 0;
-    str = (char *)malloc(sizeof(char) * (size + 1));
-    if (!str)
-        return (NULL);
-    while (size)
+	char    *str;
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	while (size >= 0)
 	{
-        str[size] = '\0';
+		str[(int) size] = '\0';
 		size--;
 	}
-    return (str);
+	return (str);
 }
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    size_t  slen;
-    char    *str;
-    int     count;
-    if (!s || !f)
-        return (NULL);
-    count = 0;
-    slen = ft_strlen(s);
-    str = dinamic_str(slen);
-    if (!str)
-        return (NULL);
-    while(s[count])
-    {
-        str[count] = f(count, s[count]);
-        count++;
-    }
-    str[count] = '\0';
-    return (str);
+	char    *str;
+	int     count;
+
+	count = 0;
+	if (!s || !f)
+		return (NULL);
+	str = dinamic_str(ft_strlen(s));
+	if (!str)
+		return (NULL);
+	while(s[count])
+	{
+		str[count] = f(count, s[count]);
+		count++;
+	}
+	str[count] = '\0';
+	return (str);
 }
