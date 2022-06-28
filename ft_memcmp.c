a863a6e3fe466808f0807d623b434dd90da52373
Manuel Ignacio Luis Fuentes <mluis-fu@c3r5s3.42madrid.com>
2022-06-28 13:10:23 +0200

    finish n2

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mluis-fu <mluis-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 17:26:49 by marvin            #+#    #+#             */
-/*   Updated: 2022/06/28 13:06:15 by mluis-fu         ###   ########.fr       */
+/*   Updated: 2022/06/28 13:09:44 by mluis-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,3 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 	}
 	return (0);
 }
-
