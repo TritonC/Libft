ec2d1f4f5e2b684da0405d68b60c4aef240b5185
Manuel Ignacio Luis Fuentes <mluis-fu@c1r10s3.42madrid.com>
2022-07-07 16:33:50 +0200

    lets go

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mluis-fu <mluis-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 14:53:30 by mluis-fu          #+#    #+#             */
-/*   Updated: 2022/06/27 16:08:45 by mluis-fu         ###   ########.fr       */
+/*   Updated: 2022/07/07 16:33:21 by mluis-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	first = NULL;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
+		new = ft_lstnew((f(lst->content));
 		if (!new)
-		{
-			if (first)
-				ft_lstclear(&first, del);
-			return (NULL);
-		}
+			ft_lstclear(&first, del);
 		ft_lstadd_back(&first, new);
 		lst = lst->next;
 	}
